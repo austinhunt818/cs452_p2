23de0492e6532b799c210064589dd328f6b8b2d0
Austin Hunt <122411917+austinhunt818@users.noreply.github.com>
2025-02-19 00:14:05 +0000

    started trimwhite

--- src/lab.c
+++ src/lab.c
@@ -9,49 +9,48 @@ void print_version() {
     printf("Version 1.0.0\n");
 }
 
-// int main(int argc, char *argv[]) {
-//     int opt;
-//     while ((opt = getopt(argc, argv, "v")) != -1) {
-//         switch (opt) {
-//             case 'v':
-//                 print_version();
-//                 return 0;
-//             default:
-//                 fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
-//                 return 1;
-//         }
-//     }
-//     return 0;
-// }
-
 char *get_prompt(const char *env) {
+    UNUSED(env);
     return NULL;
 }
 
 int change_dir(char **dir) {
+    UNUSED(dir);
     return -1;
 }
 
 char **cmd_parse(char const *line) {
+    UNUSED(line);
     return NULL;
 }
 
 void cmd_free(char **line) {
+    UNUSED(line);
 }
 
 char *trim_white(char *line) {
-    return NULL;
+    char* trimmed;
+    int i = 0;
+    while(line[i] != '\0') {
+        if (!isspace(line[i])) {
+            trimmed += line[i];
+        }
+    }
+    return trimmed;
 }
 
-bool do_builtin(struct shell *sh, char **argv) {
-
+bool do_builtin(struct shell *sh, char **argv) {   
+    UNUSED(sh);
+    UNUSED(argv);
     return 0==1;
 }
 
 void sh_init(struct shell *sh) {
+    UNUSED(sh);
 }
 
 void sh_destroy(struct shell *sh) {
+    UNUSED(sh);
 }
 
 void parse_args(int argc, char **argv) {
